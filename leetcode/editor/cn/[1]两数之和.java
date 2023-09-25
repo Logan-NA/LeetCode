1ed1c668b2c98444d3f93375da30785f508de578
Logan-NA <2570354931@qq.com>
2023-09-25 19:29:01 +0800

    两数之和

--- leetcode/editor/cn/[1]两数之和.java
+++ leetcode/editor/cn/[1]两数之和.java
@@ -49,6 +49,14 @@
 //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> hashmap = new HashMap<Integer,Integer>();
+        for(int i = 0;i < nums.length;i++){
+            if(hashmap.containsKey(target-nums[i])){
+                return new int[]{i,hashmap.get(target-nums[i])};
+            }
+            hashmap.put(nums[i],i);
+        }
+        return new int[0];
 
 
     }
